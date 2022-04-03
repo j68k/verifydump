e07b12fc2adc023c6ddb76ec0674bde5459f0d7b
j68k <99551701+j68k@users.noreply.github.com>
2022-04-03 19:19:17 +0100

    Switched from having a '--quiet' option to having a '--verbose' option with the opposite effect

--- verifydump/console.py
+++ verifydump/console.py
@@ -7,12 +7,12 @@ from .verify import load_dat, verify_dumps
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--quiet", action=argparse.BooleanOptionalAction, default=False)
+    arg_parser.add_argument("--verbose", action=argparse.BooleanOptionalAction, default=False)
     arg_parser.add_argument("dat_file")
     arg_parser.add_argument("dump_file_or_folder", nargs="+")
     args = arg_parser.parse_args()
 
-    logging.basicConfig(format="%(message)s", level=logging.WARNING if args.quiet else logging.DEBUG)
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG if args.verbose else logging.INFO)
 
     load_dat(pathlib.Path(args.dat_file))
     verify_dumps([pathlib.Path(i) for i in args.dump_file_or_folder])
