5afca9fe13e20110c7858dd92cb55cb1f6727895
j68k <99551701+j68k@users.noreply.github.com>
2022-05-09 00:10:10 +0100

    Stopped the command-line tool from showing an exception message when interrupted with Ctrl-C

--- verifydump/console.py
+++ verifydump/console.py
@@ -21,42 +21,47 @@ def handle_common_args(args):
 
 
 def verifydump_main():
-    arg_parser = arg_parser_with_common_args()
-    arg_parser.add_argument("--allow-cue-file-mismatches", action=argparse.BooleanOptionalAction, default=False)
-    arg_parser.add_argument("dat_file", metavar="dat_file_or_zip")
-    arg_parser.add_argument("dump_file_or_folder", nargs="+")
-    args = arg_parser.parse_args()
-
-    handle_common_args(args)
-
     try:
-        dat = load_dat(pathlib.Path(args.dat_file))
-    except DatParsingException as e:
-        print(f"Error when parsing Datfile: {e}", file=sys.stderr)
+        arg_parser = arg_parser_with_common_args()
+        arg_parser.add_argument("--allow-cue-file-mismatches", action=argparse.BooleanOptionalAction, default=False)
+        arg_parser.add_argument("dat_file", metavar="dat_file_or_zip")
+        arg_parser.add_argument("dump_file_or_folder", nargs="+")
+        args = arg_parser.parse_args()
+
+        handle_common_args(args)
+
+        try:
+            dat = load_dat(pathlib.Path(args.dat_file))
+        except DatParsingException as e:
+            print(f"Error when parsing Datfile: {e}", file=sys.stderr)
+            sys.exit(1)
+        except Exception as e:
+            print(f"Error reading Datfile: {e}")
+            sys.exit(1)
+
+        (verified_games, errors) = verify_dumps(dat, [pathlib.Path(i) for i in args.dump_file_or_folder], show_command_output=args.show_command_output, allow_cue_mismatches=args.allow_cue_file_mismatches, extra_cue_source=pathlib.Path(args.extra_cue_source) if args.extra_cue_source else None)
+
+        if len(verified_games) > 1:
+            print(f"Successfully verified {len(verified_games)} dumps")
+
+        if len(errors) > 0:
+            print(f"{len(errors)} dumps had errors:" if len(errors) > 1 else "1 dump had an error:", file=sys.stderr)
+
+            for error in errors:
+                if isinstance(error, ConversionException):
+                    print(f'Failed to process "{error.converted_file_path}" to verify it: {error}', file=sys.stderr)
+                    if error.tool_output:
+                        print(error.tool_output, end="", file=sys.stderr)
+                elif isinstance(error, VerificationException):
+                    print(error, file=sys.stderr)
+                else:
+                    raise error  # wut?
+
+        sys.exit(1 if len(errors) > 0 else 0)
+
+    except KeyboardInterrupt:
+        # This handler just stops Python from outputting a potentially-confusing exception message when interrupted with Ctrl-C.
         sys.exit(1)
-    except Exception as e:
-        print(f"Error reading Datfile: {e}")
-        sys.exit(1)
-
-    (verified_games, errors) = verify_dumps(dat, [pathlib.Path(i) for i in args.dump_file_or_folder], show_command_output=args.show_command_output, allow_cue_mismatches=args.allow_cue_file_mismatches, extra_cue_source=pathlib.Path(args.extra_cue_source) if args.extra_cue_source else None)
-
-    if len(verified_games) > 1:
-        print(f"Successfully verified {len(verified_games)} dumps")
-
-    if len(errors) > 0:
-        print(f"{len(errors)} dumps had errors:" if len(errors) > 1 else "1 dump had an error:", file=sys.stderr)
-
-        for error in errors:
-            if isinstance(error, ConversionException):
-                print(f'Failed to process "{error.converted_file_path}" to verify it: {error}', file=sys.stderr)
-                if error.tool_output:
-                    print(error.tool_output, end="", file=sys.stderr)
-            elif isinstance(error, VerificationException):
-                print(error, file=sys.stderr)
-            else:
-                raise error  # wut?
-
-    sys.exit(1 if len(errors) > 0 else 0)
 
 
 def convertdump_main():
